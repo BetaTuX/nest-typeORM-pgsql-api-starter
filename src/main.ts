1f1297879efb829788edac75da2a0c62531defc0
Argann BAY-EYMERE <argann.bay-eymere@epitech.eu>
2021-12-24 19:10:12 +0100

    + Added Helmet support

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { QueryFailedExceptionFilter } from './filters/queryFailedException.filter';
 import { EntityNotFoundExceptionFilter } from './filters/entityNotFound.filter';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
   app.useGlobalFilters(
     new QueryFailedExceptionFilter(),
     new EntityNotFoundExceptionFilter(),
